053940dec6c7f9df424d814cf9ac3f3b71327fed
Peter <peter@quantr.hk>
2026-06-06 17:05:56 +0800

    dev

--- obj_dir/sim_main.d
+++
@@ -1,9 +0,0 @@
-sim_main.o: ../tb/sim_main.cpp \
-  /opt/homebrew/Cellar/verilator/5.048/share/verilator/include/verilated.h \
-  /opt/homebrew/Cellar/verilator/5.048/share/verilator/include/verilated_config.h \
-  /opt/homebrew/Cellar/verilator/5.048/share/verilator/include/verilatedos.h \
-  /opt/homebrew/Cellar/verilator/5.048/share/verilator/include/verilated_types.h \
-  /opt/homebrew/Cellar/verilator/5.048/share/verilator/include/verilated_funcs.h \
-  /opt/homebrew/Cellar/verilator/5.048/share/verilator/include/verilated_vcd_c.h \
-  /opt/homebrew/Cellar/verilator/5.048/share/verilator/include/verilated_trace.h \
-  Vopen8_top.h
